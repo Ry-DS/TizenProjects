7ff4f5649bee8f09b1d42a3ee1463e6b8041e375
Ryan.S <ryan2002.rs@gmail.com>
2018-11-13 18:24:19 +1100

    Trying Virtual List

---
+++ PTV4Gear/js/data.js
@@ -0,0 +1,15 @@
+var DATA = 
+[
+   {NAME:"Nearby", CLASSES:["ui-listview-divider"], HTML:"Nearby"},
+   {NAME:"Williams Landing",CLASSES:["li-has-multiline","li-has-thumb-left"],HTML:"<a href=\"contents/PTV/select_route.html\">Williams Landing\
+					<span class=\"ui-li-sub-text li-text-sub\">Bvvvvvvus stop in</span>\
+					<span class=\"ui-li-sub-text li-text-sub\">xxxx</span>\
+					<img src=\"../../../css/images/PTV/ptv_bus.png\" class=\"ui-li-thumb-left\">\
+					</a>"},
+   {NAME:"Open Elements",CLASSES:["li-has-multiline","li-has-thumb-left"],HTML:"<a href=\"index-old.html\">Open Elements\
+					<span class=\"ui-li-sub-text li-text-sub\">Bus stgggggop in</span>\
+					<span class=\"ui-li-sub-text li-text-sub\">xxxx</span>\
+					<img src=\"../../../css/images/PTV/ptv_train.png\" class=\"ui-li-thumb-left\">\
+					</a>"}
+   /* Other list items */
+]
