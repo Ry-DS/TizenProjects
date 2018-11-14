0d09b4e91759a2771716dfd9e85d2894b303ad1c
Ryan.S <ryan2002.rs@gmail.com>
2018-11-14 22:44:24 +1100

    Got a refresh footer button working

--- PTV4Gear/js/location.js
+++ PTV4Gear/js/location.js
@@ -1,31 +1,36 @@
 var coords;//global coords var. Should have coords of user if found
+var running=false;
 document.addEventListener("pageshow", function () {
-	
+	if(running){
+		return;
+	}else running=true;
 	let list=$('#nearbyList');
 	let loop=function loop(){
-		
+		$('#refreshNearby').remove();
 		listEdit.edit('nearbyList',()=>
-		list.append('<li class=\"ui-li-grid\"> <div>Locating</div> <div class=\"ui-processing\"></div></li>'
+		list.append('<li class=\"ui-li-grid\"> <a href=\"index-old.html\"><div>Locating</div></a> <div class=\"ui-processing\"></div></li>'
 				));//processing animation
-		list.append('<li class=\"ui-li-grid\"> <div>Locating</div> <div class=\"ui-processing\"></div></li>'
-		));//processing animation
-list.append('<li class=\"ui-li-grid\"> <div>Locating</div> <div class=\"ui-processing\"></div></li>'
-));//processing animation
-list.append('<li class=\"ui-li-grid\"> <div>Locating</div> <div class=\"ui-processing\"></div></li>'
-));//processing animation
+		
 
 		navigator.geolocation.getCurrentPosition((pos)=>{
 			coords=pos.coords;
 			
 			
 			
-			listEdit.edit('nearbyList',()=>{
+			/*listEdit.edit('nearbyList',()=>{
 			list.append('<li><a id=\"refreshNearby\">Refresh</a></li>');
 			document.getElementById('refreshNearby').onclick=()=>{
 				loop();
 				
 			};
-			});
+			});*/
+			$('#main').append('<footer class=\"ui-footer ui-bottom-button ui-fixed\">\
+					<button class=\"ui-btn\" id=\"refreshNearby\">Refresh</button>\
+					</footer>');
+			document.getElementById('refreshNearby').onclick=()=>{
+				loop();
+				
+			};
 			
 			
 		
